4c4b5cdaf9c68c914559ef9447240ca32f0fefc6
SazontovDmitriy <90573392+SazontovDmitriy@users.noreply.github.com>
2021-09-12 22:22:24 +0300

    Add files via upload

---
+++ zadanie1.py
@@ -0,0 +1,13 @@
+import math
+b: str = input ("Длина первой стороны b=   ")
+C: str = input ("Длина второй стороны c=  ")
+d: str = input ("Введите величину угла d в радианах= ")
+from math import pi
+d: str =  (float(d))*(180/pi)
+import math as m
+a= math.sqrt((pow(float(C), 2)) + (pow(float(b), 2)) - (2 * (float(b)) * (float(C)) * (math.cos(float(d)))))
+from math import cos
+math.cos(float(d))
+print("d=", math.cos(float(d)))
+print("a=", a)
+
